7d9525bdb9e0c1bf729438a31a03a16a086c26c8
Arthur ANdrade <arthurfelandrade@gmail.com>
2020-03-07 18:42:57 -0300

    Adicionado ordenação, coloração, e estilo em linha

--- teste/view/MainWindow.java
+++ teste/view/MainWindow.java
@@ -1,62 +1,180 @@
 package view;
 
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.Font;
 import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerNumberModel;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import javax.swing.ComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
 
 public class MainWindow extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private Integer flagThread = 0;
 
-    MyPanel pCanvas;
-    JSpinner sQuantity;
-    
-    private void createWindow() {
-        this.setPreferredSize(new Dimension(618, 726));
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setVisible(true);
-        this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.PAGE_AXIS));
-        JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        JLabel titulo = new JLabel("Ordenação");
-        titulo.setFont(new Font("Serif", Font.BOLD, 24));
-        topPanel.add(titulo);
-        topPanel.setMaximumSize(new Dimension(640, 50));
-        this.add(topPanel);
-        
-        JPanel middlePanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-        sQuantity = new JSpinner(new SpinnerNumberModel(25, 1, 2000, 1));
-        sQuantity.setToolTipText("Quantity of numbers to sort");
-        middlePanel.add(sQuantity);
-        JButton bShuffle = new JButton("Shuffle");
-        
-        
-        
-        bShuffle.addActionListener(ae -> pCanvas.shuffle());
-        middlePanel.add(bShuffle);
-        middlePanel.setMaximumSize(new Dimension(640, 50));
-        this.add(middlePanel);
+	public MainWindow() {
+		super("Ordenação");
+                
+		setPreferredSize(new Dimension(900, 900));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		setLayout(new BoxLayout(this.getContentPane(), BoxLayout.PAGE_AXIS));
+                
+		JPanel topPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+		JLabel titulo = new JLabel("Ordenação");
+		titulo.setFont(new Font("Serif", Font.BOLD, 24));
+		topPanel.add(titulo);
+		topPanel.setMaximumSize(new Dimension(300, 300));
+		add(topPanel);
+                
+                JPanel configPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+                configPanel.setPreferredSize(new Dimension(200,80));
+                JLabel lbRange = new JLabel("Range: ");
+                JTextField tfRange = new JTextField();
+                tfRange.setPreferredSize(new Dimension(60, 20));
+                JLabel lbDelay = new JLabel("Delay: ");
+                JTextField tfDelay = new JTextField();
+                tfDelay.setPreferredSize(new Dimension(60, 20));
+                JButton btGerarArray = new JButton("Gerar array");
+                JButton btOrdenar = new JButton("Ordenar");
+                JButton btParar = new JButton("Parar");
+                btParar.setEnabled(false);
+                
+                String[] itensAparences = new String[2];
+                itensAparences[0] =  "Bars";
+                itensAparences[1] =  "Line";
+                JComboBox cbOptAparences = new JComboBox(itensAparences);
+                
+                
+                
+                String[] itensOrders = new String[2];
+                itensOrders[0] = "SelectionSort";
+                itensOrders[1] = "InsertionSort";
+                JComboBox cbOptOrders = new JComboBox(itensOrders);
+               
+                
+                configPanel.add(lbRange);
+                configPanel.add(tfRange);
+		configPanel.add(lbDelay);
+                configPanel.add(tfDelay);
+                configPanel.add(cbOptOrders);
+                configPanel.add(cbOptAparences);
+                configPanel.add(btGerarArray);
+                configPanel.add(btOrdenar);
+                //configPanel.add(btParar);
+                
+                configPanel.setMaximumSize(new Dimension(900, 900));
+                add(configPanel);
+                
+                MyPanel pCanvas = new MyPanel(100);
+		add(pCanvas);
+                
+                cbOptAparences.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent ae) {
+                        String option = cbOptAparences.getSelectedItem().toString();
+                        pCanvas.changeAparence(option);
+                         
+                    }
+                
+                });
+                
+                
+                btGerarArray.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        try{
+                            pCanvas.makeArray(Integer.parseInt(tfRange.getText()));
+                        }catch(java.lang.NumberFormatException ex){
+                            tfRange.setText("100");
+                            pCanvas.makeArray(Integer.parseInt(tfRange.getText()));
+                        }
+                        
+                   }
+                 });
+                
+                 
+                 
+                
+                
+                
+                
+                btOrdenar.addActionListener(new ActionListener() {
+                    
+                    public void actionPerformed(ActionEvent e) {
+                        flagThread = 0;
+                        
+                        
+                        
+                        Thread ordenar = new Thread(new Runnable() {
+                        @Override
+                        public void run(){
 
-        pCanvas = new MyPanel(this);
-        this.add(pCanvas);
+                            btGerarArray.setEnabled(false);
+                            btOrdenar.setEnabled(false);
+                            cbOptOrders.setEnabled(false);
+                            btParar.setEnabled(true);
 
-        this.pack();
-    }
+                            int delay = 100;
+                            try{
+                                delay = Integer.parseInt(tfDelay.getText());
+                            }catch(java.lang.NumberFormatException ex){
+                                tfDelay.setText("100");
+                                delay =  Integer.parseInt(tfDelay.getText());
+                            }finally{
+                                switch(cbOptOrders.getSelectedItem().toString()){
+                                    case "SelectionSort":
+                                        pCanvas.bubbleSort(delay);
+                                        break;
+                                    case "InsertionSort":
+                                        pCanvas.insertionSort(delay);
+                                        break;
+                                }
+                            }
+                            btGerarArray.setEnabled(true);
+                            btOrdenar.setEnabled(true);
+                            cbOptOrders.setEnabled(true);
+                            btParar.setEnabled(false);
 
-    public JSpinner getsQuantity() {
-        return sQuantity;
-    }
-    
-    public MainWindow() {
-        super("Sorting Algorithms");
-    }
-    
-    public static void main(String[] args) {
-        MainWindow w = new MainWindow();
-        w.createWindow();
-    }
+                        }// end run()
+
+                     });//ende thread
+                        
+
+                        ordenar.start();
+
+                   }
+                });
+                
+                
+                btParar.addActionListener(new ActionListener() {
+                    
+                    public void actionPerformed(ActionEvent e) {
+                        
+                        flagThread = 1;
+                        btGerarArray.setEnabled(true);
+                        btOrdenar.setEnabled(true);
+                        btParar.setEnabled(false);
+
+                   }
+                });
+                
+		
+		pack();
+	}
+	
+	public static void main(String[] args) {
+		MainWindow w = new MainWindow();
+	}
 }
--- teste/view/MyPanel.java
+++ teste/view/MyPanel.java
@@ -2,64 +2,163 @@ package view;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import javax.swing.JPanel;
 
 public class MyPanel extends JPanel {
+    
 
-    MainWindow frame;
-    protected List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
-    
-    public MyPanel(MainWindow mw) {
-        this.frame = mw;
-    }
-    
-    private void createRandomizedArray(int qtd) {
-        this.numbers = new ArrayList(qtd);
-        for(int i = 1; i <= qtd; i++) {
-            this.numbers.add(i);
+        private List<Integer> numbers;
+        private String aparence = "Bars";
+
+        public String getAparence() {
+            return aparence;
         }
-        Collections.shuffle(numbers);
-    }
-    
-    public void shuffle() {
-        int quantity = (Integer) this.frame.getsQuantity().getValue();
-        this.createRandomizedArray(quantity);
-        repaint();
-    }
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        g.clearRect(0, 0, this.getWidth(), this.getHeight());
-        g.setColor(Color.black);
-        g.fillRect(0, 0, this.getWidth(), this.getHeight());
-        int outerSpace = 5;
-        int innerSpace = 1;
-        int QTD = numbers.size();
-        int width = (this.getWidth() - 2 * outerSpace - (QTD - 1) * innerSpace) / QTD;
-        int height = (this.getHeight() - 2 * outerSpace) / QTD;
-        g.setColor(Color.white);
-        
-        
-        int[] x = new int[QTD];
-        int[] y = new int[QTD];
-        
-        for (int idx = 0; idx < QTD; idx++) {
-            int n = numbers.get(idx);
-            x[idx] = idx * (width + innerSpace) + outerSpace;
-            y[idx] = this.getHeight() - n * height - outerSpace;
+        public void changeAparence(String aparence) {
+            this.aparence = aparence;
+            repaint();
+        }
+    
+        MyPanel(int range){
+            super();
+
+            this.numbers  = IntStream.range(1, range+1).boxed().collect(Collectors.toList());
         }
         
-        g.drawPolyline(x, y, QTD);
+        private int range(){
+           return this.numbers.size();
+        }
+        
+        public void makeArray(int range){
+            this.numbers  = IntStream.range(1, range+1).boxed().collect(Collectors.toList());
+            Collections.shuffle(this.numbers);
+            paintComponent(getGraphics());  
+            
+        }
+        
+        public void bubbleSort(int delay){
+           
+                    //Bubble Sort
+                    for(int i=0; i < numbers.size(); i++) {
+                      for(int j=i+1; j < numbers.size(); j++) {
+                          Integer f1 =numbers.get(i);
 
+                          Integer f2 =numbers.get(j);
+
+
+                          refreshWithColor(getGraphics(), i, j);
+                          try { Thread.sleep (delay); } catch (InterruptedException ex) {}
+
+                          if(f1.compareTo(f2) > 0) {
+                            numbers.set(i, f2);
+                            numbers.set(j, f1);
+
+
+                          }
+
+                        paintComponent(getGraphics());  
+
+                      }
+
+                    }
+            
+          
+        }
+        
+        public void insertionSort(int delay){
+            
+            
+          
+                //Inner sort
+                for (int j = 1; j < range(); j++) {
+                     int current = numbers.get(j);
+                     int i = j-1;
+
+                     while ((i > -1) && (numbers.get(i) > current)) {
+
+                         
+                         try { Thread.sleep (delay); } catch (InterruptedException ex) {}
+
+
+                         int tst = numbers.get(i);
+                         
+                         numbers.set(i, numbers.get(i+1));
+                         numbers.set(i+1, tst);
+                         refreshWithColor(getGraphics(), i, i-1);
+
+                         i--;
+
+
+                     }
+
+
+                     numbers.set(i+1, current);
+
+
+                 }
+                
+                
+                paintComponent(getGraphics());  
+                
+               
+        }
         
         
-//        for (int idx = 0; idx < QTD; idx++) {
-//            int n = numbers.get(idx);
-//            g.fillRect(idx * (width + innerSpace) + outerSpace, this.getHeight() - n * height - outerSpace, width, n * height);
-//        }
-    }
+        private void refreshWithColor(Graphics g, int k, int j){
+            
+            g.setColor(Color.black);
+            g.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+            int outerSpace = 5;
+            int innerSpace = 1;
+            int QTD = this.range();
+            int width = (this.getWidth() - 2 * outerSpace - (QTD - 1) * innerSpace) / QTD;
+            int height = (this.getHeight() - 2 * outerSpace) / QTD;
+            
+            switch(this.aparence){
+                case "Bars":
+                    for(int i = 0; i< QTD; i++) {
+                    if(i == k || i == j){
+                        g.setColor(Color.red);
+                    }else{
+                        g.setColor(new Color(50, ((255/this.range())*this.numbers.get(i) ), (255 - ((255/this.range())*this.numbers.get(i) ))));
+                    }
+                    int n = this.numbers.get(i);
+                    g.fillRect(i * (width + innerSpace) + outerSpace, this.getHeight() - n * height - outerSpace, width, n * height);
+                    }
+                    break;
+                    
+
+                case "Line":
+                    
+                    g.setColor(Color.white);
+                    int[] x = new int[QTD];
+                    int[] y = new int[QTD];
+
+                    for (int idx = 0; idx < QTD; idx++) {
+                        int n = numbers.get(idx);
+                        x[idx] = idx * (width + innerSpace) + outerSpace;
+                        y[idx] = this.getHeight() - n * height - outerSpace;
+                    }
+
+                    g.drawPolyline(x, y, QTD);
+                    break;
+            
+            } 
+            
+        }
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		refreshWithColor(g, -1, -1);
+		
+	}
+        
+        
 }
